e20a82d15d2410c6ccbf7ca30b81a845af7f9910
arshkhanolkar <arshkhanolkar@gmail.com>
2026-08-26 16:05:08 +0530

    Foundationfr the frotnend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      <h1 className="p-10 text-4xl font-bold">
+        Airfare Index India
+      </h1>
+    </div>
+  )
+}
+
+export default App
